872767e7693e59a80ed25927ee93c4bb61b67421
Magne Halvorsen <maghal@samfundet.no>
2021-08-11 18:39:39 +0200

    Added styling and client side validation

--- web/js/functions.js
+++ web/js/functions.js
@@ -1,18 +1,95 @@
-function saveInfo(){
+function saveInfo() {
   const name = document.getElementById("input-name");
   const tlf = document.getElementById("input-tlf");
   const mail = document.getElementById("input-mail");
+  const fødselsnummer = document.getElementById("input-fødselsnummer");
 
   let storage = window.localStorage;
 
   storage.setItem("name", name.value);
   storage.setItem("tlf", tlf.value);
-  sotrage.setItem("mail", mail.value);
+  storage.setItem("mail", mail.value);
+  storage.setItem("fødselsnummer", fødselsnummer.value);
 }
 
-function validateForm(){
-  // TODO: Check stuff?
+function loadInfo() {
+  const name = document.getElementById("input-name");
+  const tlf = document.getElementById("input-tlf");
+  const mail = document.getElementById("input-mail");
+  const fødselsnummer = document.getElementById("input-fødselsnummer");
+
+  let storage = window.localStorage;
+
+  name.value = storage.getItem("name");
+  tlf.value = storage.getItem("tlf");
+  mail.value = storage.getItem("mail");
+  fødselsnummer.value = storage.getItem("fødselsnummer");
+}
+
+function validateFødselsnummer() {
+  const fødselsnummer = document.getElementById("input-fødselsnummer");
+  number = fødselsnummer.value.toString().split("");
+  k1 =
+    11 -
+    ((3 * number[0] +
+      7 * number[1] +
+      6 * number[2] +
+      1 * number[3] +
+      8 * number[4] +
+      9 * number[5] +
+      4 * number[6] +
+      5 * number[7] +
+      2 * number[8]) %
+      11);
+  k2 =
+    11 -
+    ((5 * number[0] +
+      4 * number[1] +
+      3 * number[2] +
+      2 * number[3] +
+      7 * number[4] +
+      6 * number[5] +
+      5 * number[6] +
+      4 * number[7] +
+      3 * number[8] +
+      2 * number[9]) %
+      11);
+  if (number.length == 11 && number[9] == k1 && number[10] == k2) {
+    fødselsnummer.setCustomValidity("");
+    return true;
+  }
+  fødselsnummer.setCustomValidity("Invalid input");
+  return false;
+}
+
+function validateRadios() {
+  let messageBox = document.getElementById("input-message");
+  messageBox.innerHTML = "";
+  messageBox.style.display = "none";
+
+  let timeRadios = document.getElementsByName("time");
+  let radioValid = false;
+  Array.from(timeRadios).forEach((radio) => {
+    if (radio.checked) {
+      radioValid = true;
+    }
+  });
+  if(!radioValid){
+    messageBox.innerText = "Du må velge et tidspunkt.";
+    messageBox.className = "error";
+    messageBox.style.display = "block";
+  }
+  return radioValid;
+}
+
+function validateForm() {
+
+  if (!validateRadios()){
+    return false;
+  }
+  if (!validateFødselsnummer()) {
+    return false;
+  }
   saveInfo();
   return true;
 }
-
