f86b66ba9e4d73e5110d06af0b632550fca779fd
Magne Halvorsen <maghal@samfundet.no>
2021-08-16 12:18:07 +0200

    Added masss form print

--- web/admin/person.php
+++ web/admin/person.php
@@ -9,13 +9,28 @@
 require_once "../../include/init.php";
 $arguments["title"] = "Hurtigtest";
 
-$query = "
-  SELECT * FROM hurtigtest.paameldinger WHERE id = $1;
-";
-$params = [$_GET["id"]];
-$result = pg_query_params($query, $params);
+$arguments["personer"] = [];
+if(isset($_GET["id"])){
+  $query = "
+    SELECT * FROM hurtigtest.paameldinger WHERE id = $1;
+  ";
+  $params = [$_GET["id"]];
+  $result = pg_query_params($query, $params);
 
-$arguments["person"] = pg_fetch_array($result);
+  $arguments["personer"][] = pg_fetch_array($result);
+} else if (isset($_GET["tidspunkt"])) {
+  $query = "
+    SELECT * FROM hurtigtest.paameldinger WHERE tidspunkt_id = $1;
+  ";
+  $params = [$_GET["tidspunkt"]];
+  $result = pg_query_params($query, $params);
+
+  while($row = pg_fetch_array($result)){
+    $arguments["personer"][] = $row;
+  }
+} else {
+  $arguments["personer"][] = "";
+}
 
 $twig->load("person.twig")->display($arguments);
 ?>
